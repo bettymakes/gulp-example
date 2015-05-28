b2cd69a5e28242802f5f92c4c75b0e0398ad4932
betty <makewithbetty@gmail.com>
2015-05-28 02:29:07 -0400

    Moved scripts related task into its own task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 var gulp    = require('gulp'),
     uglify  = require('gulp-uglify');
 
-// Creating default task
-gulp.task('default', function(){
-
-  // Loads any '.js' files in the /js directory
+// Scripts Task 
+// Uglifies JS
+gulp.task('scripts', function(){
+   // Loads any '.js' files in the /js directory
   gulp.src('js/*.js')
     // Minifies the code
     .pipe(uglify())
-    // Saves the minified code into build/js directory
+    // Compiles the minified code into build/js directory
     .pipe(gulp.dest('build/js'))
+});
+
+// Creating default task
+gulp.task('default', function(){
+ 
 });
