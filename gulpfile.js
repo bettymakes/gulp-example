e5378ae687e541874dfc38540f966e875d2a2085
betty <makewithbetty@gmail.com>
2015-05-28 02:35:48 -0400

    Adding a styles task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp    = require('gulp'),
     uglify  = require('gulp-uglify');
 
-// Scripts Task 
+// Task: SCRIPTS  =========================================================
 // Uglifies JS
 gulp.task('scripts', function(){
    // Loads any '.js' files in the /js directory
@@ -12,7 +12,16 @@ gulp.task('scripts', function(){
     .pipe(gulp.dest('build/js'))
 });
 
-// Creating default task
-gulp.task('default', function(){
- 
+// Task: STYLES ===========================================================
+// Testing for the interim, printing text to screen
+gulp.task('styles', function(){
+  console.log('Testing');
 });
+
+
+// Task: DEFAULT ==========================================================
+/* Note:  Instead of passing in a callback function, you can pass in an array
+          as the second argument. The array will contain the names of any tasks
+          you want to run within the 'default' task.
+*/
+gulp.task('default', ['scripts', 'styles']);
