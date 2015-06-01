3b20c8ba90c67f92b4a50166d6a17db197fc9fd4
betty <makewithbetty@gmail.com>
2015-05-31 23:24:26 -0400

    Express

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,40 @@
 var express     = require('express'),
     gulp        = require('gulp'),
-    livereload  = require('gulp-livereload'),
     plumber     = require('gulp-plumber'),
     sass        = require('gulp-sass'),
+    tinyLr      = require('tiny-lr')(),
     uglify      = require('gulp-uglify');
+
+// Middleware
+var connectLivereload = require('connect-livereload')();
+
+// CONSTANTS
+var EXPRESS_PORT      = 4000,
+    EXPRESS_ROOT      = './build',
+    LIVERELOAD_PORT   = 35729;
 
 // Fn: Starting Express Server
 function startExpress(){
+  console.log("starting express server");
   var app = express();
-  app.use(express.static('gulp-tutorial'))
-  app.listen(4000);
+  app.use(connectLivereload);
+  app.use(express.static(EXPRESS_ROOT))
+  app.listen(EXPRESS_PORT);
+}
+
+// Fn: Starting Tiny-Lr
+function startLivereload(){
+  tinyLr.listen(LIVERELOAD_PORT);
+}
+
+// Fn: Notify Livereload
+function notifyLivereload(event){
+  var fileName = require('path').relative(EXPRESS_ROOT, event.path);
+  tinyLr.changed({
+    body: {
+      files: [fileName]
+    }
+  });
 }
 
 // Fn: Logging error
@@ -17,6 +42,17 @@ function errorLog(error){
   console.error(error);
   this.emit('end');
 };
+
+// Task: EXPRESS  =========================================================
+gulp.task('expressServer', function(){
+  console.log('EXPRESS SERVER TASK');
+  startExpress();
+});
+
+// Task: LIVERELOAD  =========================================================
+gulp.task('livereload', function(){
+  startLivereload();
+});
 
 // Task: SCRIPTS  =========================================================
 // Uglifies JS
@@ -47,31 +83,39 @@ gulp.task('styles', function(){
       // Method 2: errorLog Fn is declared up top
       .on('error', errorLog)
       // Outputs the css into build/styles directory
-      .pipe(gulp.dest('build/styles'))
-      .pipe(livereload());
+      .pipe(gulp.dest('build/styles'));
 });
 
 
 // Task: WATCH ============================================================
 // Watches JS
 gulp.task('watch', function(){
-  var server = livereload.listen();
-
   // Watches all .js files in /js directory for changes. On change, we will
   // run the 'scripts' gulp task.
   gulp.watch('js/*.js', ['scripts']);
 
   // Watches all .scss files in /styles directory for changes. On change, we will
   // run the 'styles' gulp task.
-  gulp.watch('styles/*.scss', ['styles'])
+  gulp.watch('styles/*.scss', ['styles']);
+
+  gulp.watch('index.html', ['index']);
+  gulp.watch('*.html', notifyLivereload);
 });
 
+
+gulp.task('index', function(){
+  gulp.src('index.html')
+  .pipe(gulp.dest('build'));
+});
 
 // Task: DEFAULT ==========================================================
 /* Note:  Instead of passing in a callback function, you can pass in an array
           as the second argument. The array will contain the names of any tasks
           you want to run within the 'default' task.
 */
-gulp.task('default', ['scripts', 'styles', 'watch', function(){
-  startExpress();
-}]);
+gulp.task('default', 
+  ['index', 'scripts', 'styles', 'expressServer', 'livereload', 'watch']);
+
+
+
+
